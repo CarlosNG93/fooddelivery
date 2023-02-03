fc943f07eba6a6d7ab9a160caa02280ba9994733
Carlos <carlosng1993@gmail.com>
2023-02-03 13:14:14 +0100

    feat: structure changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,43 @@
 import React from 'react';
-import Logo from './assets/logo.png';
+import Logo from '../assets/logo.png';
+import Avatar from '../assets/avatar.png';
+import { MdShoppingBasket } from 'react-icons/md';
+
 
 const Header = () => {
 	return (
 		<header className='fixed z-50 w-screen p-6 px-16'>
 			{/* DESKTOP & TABLET */}
-			<div className='hidden md:flex w-full'>
+			<div className='hidden md:flex w-full items-center justify-between'>
 				<div className='flex items-center gap-2'>
 					<img src={Logo} className="w-8 object-cover" alt="logo" />
 					<p className="text-headingColor text-xl font-bold">City</p>
+				</div>
 
-					<ul className=''>
-						<li>Home</li>
-						<li>Menu</li>
-						<li>About Us</li>
-						<li>Service</li>
+				<div className='flex items-center gap-8'>
+					<ul className='flex items-center gap-8 '>
+						<li className='text-base text-textColor hover:text-headingColor duration-100 tarnsition-all ease-in-out cursor-pointer'>Home</li>
+						<li className='text-base text-textColor hover:text-headingColor duration-100 tarnsition-all ease-in-out cursor-pointer'>Menu</li>
+						<li className='text-base text-textColor hover:text-headingColor duration-100 tarnsition-all ease-in-out cursor-pointer'>About Us</li>
+						<li className='text-base text-textColor hover:text-headingColor duration-100 tarnsition-all ease-in-out cursor-pointer'>Service</li>
 					</ul>
+
+					<div className='relative flex items-center justify-center'>
+						<MdShoppingBasket className='text-textColor text-2xl ml-8 cursor-pointer'/>
+						<div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+							<p className='text-xs text-white font-semibold'>2</p>
+						</div>
+					</div>
+
+					<img src={Avatar} className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl' alt="userprofile" />
 				</div>
 			</div>
 
 
 			{/* MOBILE */}
-			<div className='flex md:hidden w-full h-full'></div>
+			<div className='flex md:hidden w-full h-full'>
+				
+			</div>
 		</header>
 	)
 }

e1ede521f9c047efe3728aa6c77a8c77b48dbff3
Carlos <carlosng1993@gmail.com>
2023-02-03 11:58:11 +0100

    feat: structure changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import Logo from './assets/logo.png';
 
 const Header = () => {
-  return (
-	<div className='fixed z-50 w-screen bg-slate-300 p-6 px-16'>
-		{/* DESKTOP & TABLET */}
-		<div className='hidden md:flex w-full'></div>
+	return (
+		<header className='fixed z-50 w-screen p-6 px-16'>
+			{/* DESKTOP & TABLET */}
+			<div className='hidden md:flex w-full'>
+				<div className='flex items-center gap-2'>
+					<img src={Logo} className="w-8 object-cover" alt="logo" />
+					<p className="text-headingColor text-xl font-bold">City</p>
+
+					<ul className=''>
+						<li>Home</li>
+						<li>Menu</li>
+						<li>About Us</li>
+						<li>Service</li>
+					</ul>
+				</div>
+			</div>
 
 
-		{/* MOBILE */}
-		<div className='flex md:hidden w-full h-full'></div>
-	</div>
-  )
+			{/* MOBILE */}
+			<div className='flex md:hidden w-full h-full'></div>
+		</header>
+	)
 }
 
 export default Header;

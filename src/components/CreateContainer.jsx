7cf1e09b24a4dd3ca35f66d82ba66488639adec3
Carlos <carlosng1993@gmail.com>
2023-03-01 11:32:14 +0100

    Feat: ContainerStates added

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import { MdFastfood, MdCloudUpload, MdDelete, MdFoodBank, MdAttachMoney } from 'react-icons/md';
+import { categories } from '../utils/data';
+import { deleteObject, getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '../firebase.config';
+import { actionType } from '../context/reducer';
+import { useStateValue } from '../context/StateProvider';
+
+
 
 const CreateContainer = () => {
+
+  const [title, setTitle] = useState("");
+  const [calories, setCalories] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [fields, setFields] = useState(false);
+  const [alertStatus, setAlertStatus] = useState("danger");
+  const [msg, setMsg] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [{ foodItems }, dispatch] = useStateValue();
+  
+
   return (
-	<div>CreateContainer</div>
+	<div
+		className='w-full min-h-screen flex items-center justify-center'
+	>
+		<div className='w-[90%] md:w-[50%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4'>
+
+		</div>
+	</div>
   )
 }
 

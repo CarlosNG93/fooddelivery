94ff0aa7bd8c106a85670f51de35dbdd69dec54a
Carlos <carlosng1993@gmail.com>
2023-03-01 11:49:14 +0100

    Feat: CreateContainer added

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -4,6 +4,7 @@ import { MdFastfood, MdCloudUpload, MdDelete, MdFoodBank, MdAttachMoney } from '
 import { categories } from '../utils/data';
 import { deleteObject, getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import { storage } from '../firebase.config';
+import { getAllFoodItems, saveItem } from "../utils/firebaseFunctions";
 import { actionType } from '../context/reducer';
 import { useStateValue } from '../context/StateProvider';
 
@@ -28,6 +29,53 @@ const CreateContainer = () => {
 		className='w-full min-h-screen flex items-center justify-center'
 	>
 		<div className='w-[90%] md:w-[50%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4'>
+			{fields && (
+          	<motion.p
+            	initial={{ opacity: 0 }}
+            	animate={{ opacity: 1 }}
+            	exit={{ opacity: 0 }}
+            	className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${
+              	alertStatus === "danger"
+                	? "bg-red-400 text-red-800"
+                	: "bg-emerald-400 text-emerald-800"
+            	}`}
+          	>
+            	{msg}
+          	</motion.p>
+        	)}
+
+			<div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+          		<MdFastfood className="text-xl text-gray-700" />
+          		<input
+            		type="text"
+            		required
+            		value={title}
+            		onChange={(e) => setTitle(e.target.value)}
+            		placeholder="Give me a title..."
+            		className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
+          		/>
+        	</div>
+
+			<div className="w-full">
+          		<select
+            		onChange={(e) => setCategory(e.target.value)}
+            		className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+          		>
+            	<option value="other" className="bg-white">
+              		Select Category
+            	</option>
+            	{categories &&
+              		categories.map((item) => (
+                		<option
+                  			key={item.id}
+                  			className="text-base border-0 outline-none capitalize bg-white text-headingColor"
+                  			value={item.urlParamName}
+                		>
+                  			{item.name}
+                		</option>
+              		))}
+          		</select>
+        	</div>
 
 		</div>
 	</div>
